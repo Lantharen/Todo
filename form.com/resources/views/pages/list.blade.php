63b264bd4d090a64e184e1cd846885ac4481e417
Alex <alvastarua@gmail.com>
2023-02-24 20:01:48 +0200

    Add navigation panel

--- form.com/resources/views/pages/list.blade.php
+++ form.com/resources/views/pages/list.blade.php
@@ -5,6 +5,11 @@
         <div class="row">
             <div class="col">
 
+                <div class="d-flex justify-content-between align-items-center">
+                    <h1 class="display-4">Todo List</h1>
+                    <a href="{{ route('todo.create') }}" class="btn btn-sm btn-primary">Create</a>
+                </div>
+
                 <table class="table">
 
                     <thead>
